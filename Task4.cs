760a96a00dd9e840845e42f0174425cbf2a8c997
Alex Puchek <onix720604@bk.ru>
2020-06-30 18:34:06 +0300

    Update Task4.cs

--- Task4.cs
+++ Task4.cs
@@ -27,9 +27,6 @@ namespace Task4
 				int result = FindAmountOfMoves(whiteMice, blackMice);
 				Console.WriteLine(result);
 				
-				/*int result = CountAmountOfMoves(whiteMice, blackMice);
-				Console.WriteLine(result);
-				*/
             }
             catch (Exception ex)
             {
@@ -40,76 +37,6 @@ namespace Task4
         static int FindAmountOfMoves(int whiteMiceAmount, int blackMiceAmount) 
         {
             return whiteMiceAmount * blackMiceAmount + whiteMiceAmount + blackMiceAmount;
-        }
-		
-        static int CountAmountOfMoves(int whiteMiceAmount, int blackMiceAmount)
-        {
-            int counterOfMoves = 0;
-            bool isCountFinished = false;
-            int indexOfEmptyCell = whiteMiceAmount;            
-            char[] field = new char[whiteMiceAmount + blackMiceAmount + 1];
-
-            for (int j = 0; j < field.Length; j++)
-            {
-                if (j < indexOfEmptyCell)
-                    field[j] = 'Б';
-                else if (j == indexOfEmptyCell)
-                    field[j] = ' ';
-                else
-                    field[j] = 'Ч';
-            }
-
-            while (isCountFinished != true)
-            {
-                if (indexOfEmptyCell >= 2 && field[indexOfEmptyCell - 2] == 'Б' && field[indexOfEmptyCell - 1] == 'Ч')
-                {
-                    field[indexOfEmptyCell - 2] = ' ';
-                    field[indexOfEmptyCell] = 'Б';
-                    indexOfEmptyCell -= 2;
-                }
-                else if (indexOfEmptyCell < field.Length - 2 && field[indexOfEmptyCell + 2] == 'Ч' && field[indexOfEmptyCell + 1] == 'Б')
-                {
-                    field[indexOfEmptyCell + 2] = ' ';
-                    field[indexOfEmptyCell] = 'Ч';
-                    indexOfEmptyCell += 2;
-                }
-                else if (indexOfEmptyCell != 0 && indexOfEmptyCell != field.Length - 1 && field[indexOfEmptyCell - 1] == 'Б' && field[indexOfEmptyCell + 1] == 'Ч')
-                {
-                    if (indexOfEmptyCell - 2 > -1 && field[indexOfEmptyCell - 2] == 'Ч')
-                    {
-                        field[indexOfEmptyCell] = 'Ч';
-                        field[indexOfEmptyCell + 1] = ' ';
-                        indexOfEmptyCell += 1;
-                    }
-                    else
-                    {
-                        field[indexOfEmptyCell] = 'Б';
-                        field[indexOfEmptyCell - 1] = ' ';
-                        indexOfEmptyCell -= 1;
-                    }
-                }
-                else if (indexOfEmptyCell != 0 && field[indexOfEmptyCell - 1] == 'Б')
-                {
-                    field[indexOfEmptyCell] = 'Б';
-                    field[indexOfEmptyCell - 1] = ' ';
-                    indexOfEmptyCell -= 1;
-                }
-                else if (indexOfEmptyCell != field.Length - 1 && field[indexOfEmptyCell + 1] == 'Ч')
-                {
-                    field[indexOfEmptyCell] = 'Ч';
-                    field[indexOfEmptyCell + 1] = ' ';
-                    indexOfEmptyCell += 1;
-                }
-                else
-                {
-                    isCountFinished = true;
-                    continue;
-                }
-                /*Array.ForEach(array, x => Console.Write(x));
-                Console.WriteLine();*/
-                counterOfMoves++;
-            }
-            return counterOfMoves;
-        }
+        }	      
     }
 }
